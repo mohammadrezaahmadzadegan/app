26cadefedfb1772554ff3d656684a3b7cc1852ef
mohammadrezaahmadzadegan <162569442+mohammadrezaahmadzadegan@users.noreply.github.com>
2024-03-13 21:02:31 +0330

    start delete

---
+++ models/insert.php
@@ -0,0 +1,64 @@
+<?php
+class InsertForm
+{
+    function __construct()
+    {
+        $dbHost = "localhost";
+        $dbName = "db_app";
+        $dbUser = "root";      //by default root is user name.  
+        $dbPassword = "";
+
+        $this->database = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+        date_default_timezone_set('Asia/Tehran');
+    }
+    function insertForm($text)
+    {
+        
+        $sql = 'INSERT INTO table_data (text) value (?) ';
+        $allData = $this->database->prepare($sql);
+        $allData->bindValue(1,  $text);
+
+        $allData->execute();
+        header('location: http://localhost/app/');
+
+    }
+
+    function selectForm()
+    {
+        $sql = 'SELECT * FROM table_data ';
+        $allData = $this->database->prepare($sql);
+        $allData->execute();
+        $result = $allData->fetchAll();
+        return $result;                            
+    }
+
+    function updateForm($text,$id){
+        $sql = 'UPDATE table_data SET text=? WHERE id=? ';
+        $allData = $this->database->prepare($sql);
+        $allData->bindValue(1, $text);
+        $allData->bindValue(2, $id);
+        $allData->execute();
+
+        header('location: http://localhost/app/');
+
+    }
+
+   
+    
+ 
+}
+$result = new InsertForm;
+if(isset($_POST['text'])){
+    $text = $_POST['text'];
+    $result->insertForm($text);
+}
+
+if(isset($_POST['text1']) & isset($_GET['id'])){
+    $text = $_POST['text1'];
+    $id = $_GET['id'];
+    $result->updateForm($text,$id);
+}
+
+
+
+
